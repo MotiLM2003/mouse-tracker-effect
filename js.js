babd058c7d2213ba6af70f32ba70fa40fee3e28d
Moti Elmakies <motiphone2003@gmail.com>
2020-10-29 20:05:09 +0200

    progress

--- js.js
+++ js.js
@@ -1,7 +1,24 @@
 const cursor = document.querySelector('.cursor');
-let links = document.querySelectorAll('.nav-linke li');
+let links = document.querySelectorAll('.nav-links li');
 
 document.addEventListener('mousemove', (e) => {
   cursor.style.top = `${e.clientY}px`;
   cursor.style.left = `${e.clientX}px`;
+});
+
+console.log(links);
+
+links.forEach((link) => {
+  link.addEventListener('mouseover', (e) => {
+    console.log('over');
+    cursor.classList.add('scale');
+    link.classList.add('hoverd-link');
+  });
+  console.log(link);
+
+  link.addEventListener('mouseleave', () => {
+    console.log('leave');
+    cursor.classList.remove('scale');
+    link.classList.remove('hoverd-link');
+  });
 });
